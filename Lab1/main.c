b74124151285b46b42eed0732d11cccc9194ac07
Skyye De La Riva <skyyedlr@gmail.com>
2026-02-08 12:00:33 -0700

    Debugged postfix expression output to proper format and added a function to remove whitespace

--- Lab1/main.c
+++ Lab1/main.c
@@ -25,11 +25,24 @@ void postorder(struct node *root) {
     // If root is a VAR add it to the char 'a' to get another char
     if (root->type == VAR) {
         printf("%c", 'a' + root->data);
-    } else if (root->data == CONST) {    // If root is a number, just print it.
+    } else if (root->type == CONST) {    // If root is a number, just print the operator from said type from optable.
         printf("%d", root->data);
     } else if (root->type == UNARYOP || root->type == BINARYOP) {   // If the root is UNARYOP or BINARYOP use the optable from build.c
         printf("%s", optable[root->data].symbol);
     }
+}
+
+void remove_spaces(char str[]) {
+    // Index for reading from original string and index for writing to new string (without spaces)
+    int i, j;
+    for (i = 0, j = 0; str[i] != '\0'; i++) {
+        // Check if current character is NOT a space
+        if (str[i] != ' ') {
+            str[j++] = str[i]; // If it's not a space, copy it to the new position and increment j
+        }
+    }
+    // Add the null terminator at the end
+    str[j] = '\0';
 }
 
 int main() {
@@ -38,6 +51,7 @@ int main() {
 
     while (get_str(expr1, MAXEXPRLENGTH)) {   
         printf("Expression to parse: %s\n", expr1);
+        remove_spaces(expr1);
         root = build_tree(expr1);
         printf("Postfix: ");
         postorder(root);
